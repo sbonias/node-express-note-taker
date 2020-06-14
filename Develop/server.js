13ef99b8d8a68590eeb385844891a5063e2b22f6
Sam Bonias <sbonias84@gmail.com>
2020-06-14 09:41:46 -0400

    worked on establishing htmlRoutes.js file and linked to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,15 @@
 // Dependencies
 //  requires express and links us to the express library
-var express = require("express");
+const express = require("express");
 
 // Express configuration
 //  informs node that we are creating an express server
-var app = express();
+const app = express();
 
 //  sets a port for server to run on and also sets an environmental
 //  variable PORT essentially passing a parameter
 //  Heroku won't work without the "process.env.PORT" code
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 // Establishes middleware
 //  sets up the express app to handle data parsing
@@ -21,8 +21,8 @@ app.use(express.json());
 //  the below points our server to a series of "route" files
 //  these routes give our server a "map" of how to respond when users
 //  visit or request data from various URLs
-require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
 
 // Listener
 //  The below code starts our server
